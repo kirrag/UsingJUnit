0a86de516575581ff56f3ed2fb1e6b9fed0a0f2a
Garrik Ten <ten@otux.ru>
2023-02-27 16:09:38 +0300

    Edited Main MainTest pom

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -82,8 +82,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// JSON -> Object
-		String json3 = readString("data.json");
-		System.out.println(json3);
+		String json3 = readString("");
+		//String json3 = readString("data.json");
 
 		List<Employee> list3 = jsonToList(json3);
 
